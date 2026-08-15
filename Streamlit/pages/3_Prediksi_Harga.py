d4f6605cd0e7b272bf432bfa5f03f3529cfaec0a
Selviana Putri <130084184+selvianaPutri14@users.noreply.github.com>
2026-08-15 16:28:07 +0700

    Update 3_Prediksi_Harga.py

--- Streamlit/pages/3_Prediksi_Harga.py
+++ Streamlit/pages/3_Prediksi_Harga.py
@@ -218,7 +218,7 @@ def main():
         y = np.array(y).flatten()
         
         total = len(X)
-        val_end = int(total * 0.80)
+        val_end = int(total * 0.70)
         X_test = X[val_end:]
         y_test = y[val_end:]
         
